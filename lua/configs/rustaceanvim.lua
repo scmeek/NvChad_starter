9fc0a4f19a3c7ba70f74d98e3d2158386acf5762
s-meek <s.meek@astroscale-us.com>
2025-05-22 13:40:58 -0600

    rustaceanvim - configure for linux (wsl)

--- lua/configs/rustaceanvim.lua
+++ lua/configs/rustaceanvim.lua
@@ -15,9 +15,9 @@ return {
     local extension_path = vim.fn.expand "$MASON/packages/codelldb/extension/"
     local codelldb_path = extension_path .. "adapter/codelldb"
 
-    local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
+    -- local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
     -- If you are on Linux, replace the line above with the line below:
-    -- local liblldb_path = extension_path .. "lldb/lib/liblldb.so"
+    local liblldb_path = extension_path .. "lldb/lib/liblldb.so"
 
     local cfg = require "rustaceanvim.config"
 
