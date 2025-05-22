e775d0d338b301eb97290452b14ac9a24192da6c
s-meek <s.meek@astroscale-us.com>
2025-05-22 13:29:27 -0600

    crates.nvim - update to suggested completion configuration

--- lua/configs/crates.lua
+++ lua/configs/crates.lua
@@ -1,19 +1,19 @@
+local on_attach = require("nvchad.configs.lspconfig").on_attach
+
 return {
   "saecki/crates.nvim",
   ft = { "rust", "toml" },
   tag = "stable",
   event = { "BufRead Cargo.toml" },
   config = function(_, opts)
-    opts.completion = {
-      cmp = { enabled = true },
+    opts.lsp = {
+      enabled = true,
+      on_attach = on_attach,
+      actions = true,
+      completion = true,
+      hover = true,
     }
 
     require("crates").setup(opts)
-
-    require("cmp").setup.buffer {
-      sources = {
-        { name = "crates" },
-      },
-    }
   end,
 }
